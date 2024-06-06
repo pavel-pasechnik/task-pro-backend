22688978a0c10f66b773514d8b8f56740490ba92
Pavel <Pavel.Pasechnik@gmail.com>
2024-06-06 17:00:02 +0300

    bugs fix

--- helpers/mail.js
+++ helpers/mail.js
@@ -3,17 +3,13 @@ import sgMail from '@sendgrid/mail';
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendMail = async (email, verifyToken) => {
-  try {
-    return await sgMail.send({
-      to: email,
-      from: process.env.EMAIL_SENDER,
-      subject: 'Welcome to Contacts Book!',
-      html: `To confirm your email please click on the <a href="http://localhost:${process.env.PORT}/api/users/verify/${verifyToken}">link</a>`,
-      text: `To confirm your email please open the link http://localhost:${process.env.PORT}/api/users/verify/${verifyToken}`,
-    });
-  } catch (error) {
-    throw error;
-  }
+  return await sgMail.send({
+    to: email,
+    from: process.env.EMAIL_SENDER,
+    subject: 'Welcome to Contacts Book!',
+    html: `To confirm your email please click on the <a href="http://localhost:${process.env.PORT}/api/users/verify/${verifyToken}">link</a>`,
+    text: `To confirm your email please open the link http://localhost:${process.env.PORT}/api/users/verify/${verifyToken}`,
+  });
 };
 
 export default sendMail;
--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -1,12 +1,14 @@
 import Joi from 'joi';
 
+const message = 'Помилка від Joi або іншої бібліотеки валідації';
+
 export const createUserSchema = Joi.object({
   email: Joi.string().email().required().lowercase().trim(),
   password: Joi.string()
     .pattern(new RegExp(`^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&])[A-Za-z0-9@$!%*?&]{4,}$`))
     .required(),
 }).error(errors => {
-  return new Error('Помилка від Joi або іншої бібліотеки валідації');
+  return new Error(message);
 });
 
 export const updateUserSchema = Joi.object({
@@ -17,7 +19,7 @@ export const updateUserSchema = Joi.object({
 })
   .min(1)
   .error(errors => {
-    return new Error('Помилка від Joi або іншої бібліотеки валідації');
+    return new Error(message);
   });
 
 export const loginUserSchema = Joi.object({
@@ -26,13 +28,13 @@ export const loginUserSchema = Joi.object({
     .pattern(new RegExp(`^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&])[A-Za-z0-9@$!%*?&]{4,}$`))
     .required(),
 }).error(errors => {
-  return new Error('Помилка від Joi або іншої бібліотеки валідації');
+  return new Error(message);
 });
 
 export const updateUserSubscriptionSchema = Joi.object({
   subscription: Joi.string().valid('starter', 'pro', 'business'),
 }).error(errors => {
-  return new Error('Помилка від Joi або іншої бібліотеки валідації');
+  return new Error(message);
 });
 
 export const verifyCheckSchema = Joi.object({
